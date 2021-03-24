4a03b5b6c7567f5157926bbb71f280b181b7c998
AlexMaciaFiteni <alex.macia.fiteni2@gmail.com>
2021-03-24 19:18:28 +0100

    Added example to attack HTTP server (Rasa demo in this case)

---
+++ indexTTS.js
@@ -0,0 +1,21 @@
+var myPythonScriptPath = 'MozillaTTSLib/launch_tts.py';
+const {PythonShell} = require('python-shell');
+
+var pyOptions = {
+	mode: 'text',
+	args: ['This is a test sentence']
+};
+
+var pysh = new PythonShell(myPythonScriptPath, pyOptions);
+
+pysh.on('message', function(message) {
+	console.log(message);
+});
+
+pysh.end(function(err) {
+	if(err){
+		throw err;
+	};
+
+	console.log('Finished!');
+});
