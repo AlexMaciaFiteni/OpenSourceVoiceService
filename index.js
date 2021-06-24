4ab4ee2a83e84badbd037e0c6a569e2e879020a1
AlexMaciaFiteni <alex.macia.fiteni2@gmail.com>
2021-06-24 11:13:34 +0200

    Modified index.js to be a server and receive buffers in requests

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const util = require('util');
+const http = require('http');
+const port = 8080;
 
 const callSTT = require('./helpers/callSTT.js').callSTT;
 const callTTS = require('./helpers/callTTS.js').callTTS;
 const callNLU = require('./helpers/callNLU.js').callNLU;
 
-callSTT('Useless parameter').then((msgstt) =>
+http.createServer(function(req, res)
 {
-  console.log("To process: ["+msgstt+"]");
+	let body = Buffer.alloc(0);
+	req.on('data', buffer => {
+    body = Buffer.concat([body, buffer], body.length + buffer.length) 
+  });
 
-  msgstt = 'Hi there!'; // TODO:: Don't override
-  callNLU(msgstt).then((msgnlu) =>
-  {
-    console.log('Chatbot said: '+msgnlu);
-    
-    msgnlu = 'This is a test sentence haha'; // TODO:: Don't override
-    callTTS(msgnlu).then((msgtts) =>
+	req.on('end', () => {
+    console.log('Request fully received: ' + body.length);
+
+    callSTT('Useless parameter').then((msgstt) =>
     {
-      console.log("TTS finished, check the output file");
+      console.log("To process: ["+msgstt+"]");
 
+      msgstt = 'Hi there!'; // TODO:: Don't override
+      callNLU(msgstt).then((msgnlu) =>
+      {
+        console.log('Chatbot said: '+msgnlu);
+        
+        msgnlu = 'This is a test sentence haha'; // TODO:: Don't override
+        callTTS(msgnlu).then((msgtts) =>
+        {
+          console.log("TTS finished, check the output file");
+
+        }).catch((msgerr) => {console.log(msgerr)});
+      }).catch((msgerr) => {console.log(msgerr)});
     }).catch((msgerr) => {console.log(msgerr)});
-  }).catch((msgerr) => {console.log(msgerr)});
-}).catch((msgerr) => {console.log(msgerr)});
+
+  });
+}).listen(port);
