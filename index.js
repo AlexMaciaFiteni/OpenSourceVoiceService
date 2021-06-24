10a4719405acb4420f2f08c4cc1e17b956da830c
AlexMaciaFiteni <alex.macia.fiteni2@gmail.com>
2021-06-24 14:24:40 +0200

    Server adapted to receive and send audio

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const http = require('http');
 const port = 8080;
+
+const inputFilepath = './input/prueba_stt_mono.wav';
+const outputFilepath = './tts_out.wav';
 
 const callSTT = require('./helpers/callSTT.js').callSTT;
 const callTTS = require('./helpers/callTTS.js').callTTS;
@@ -19,15 +23,21 @@ http.createServer(function(req, res)
     {
       console.log("To process: ["+msgstt+"]");
 
-      msgstt = 'Hi there!'; // TODO:: Don't override
+      // TODO:: Don't override
+      msgstt = 'Hi there!'; console.log(" > Overriding to ["+msgstt+"]");
       callNLU(msgstt).then((msgnlu) =>
       {
-        console.log('Chatbot said: '+msgnlu);
+        let parsed = JSON.parse(msgnlu);
+        msgnlu = parsed[0].text;
+        console.log('Chatbot said: ['+msgnlu+"]");
         
-        msgnlu = 'This is a test sentence haha'; // TODO:: Don't override
+        // TODO:: Don't override
+        msgnlu = 'This is a test sentence haha'; console.log(" > Overriding to ["+msgnlu+"]");
         callTTS(msgnlu).then((msgtts) =>
         {
-          console.log("TTS finished, check the output file");
+          console.log("TTS finished");
+          const readStream = fs.createReadStream(outputFilepath);
+          readStream.pipe(res);
 
         }).catch((msgerr) => {console.log(msgerr)});
       }).catch((msgerr) => {console.log(msgerr)});
