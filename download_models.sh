2d5cbf4c1f08b4973fed186a16eda1acc9250d91
AlexMaciaFiteni <alex.macia.fiteni2@gmail.com>
2021-09-05 17:44:39 +0200

    Added TTS model's config.json file to download script

--- download_models.sh
+++ download_models.sh
@@ -6,4 +6,5 @@ cd ..
 mkdir tts_models
 cd tts_models
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WXSmsl_eO4BGtJfah-nY8ARKDcsEwpW9' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1WXSmsl_eO4BGtJfah-nY8ARKDcsEwpW9" -O best_model.pth.tar && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1s7g4n-B73ChCB48AQ88_DV_8oyLth8r0' -O config.json
 cd ..
