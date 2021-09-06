8849d7e84ea279dbb16cae92a9fb24d841ca9848
AlexMaciaFiteni <alex.macia.fiteni2@gmail.com>
2021-09-06 18:19:20 +0200

    Added script to download english models

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,7 @@
 mkdir stt_models
 cd stt_models
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q9HhT8IYx5sFEcADjtLLLZ_PsrrsyYQE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1q9HhT8IYx5sFEcADjtLLLZ_PsrrsyYQE" -O modelo_stt.pbmm && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=11fHpV4y2bML9LYs2VYhG75o7_Uwm5c2Z' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=11fHpV4y2bML9LYs2VYhG75o7_Uwm5c2Z" -O kenlm_es.scorer && rm -rf /tmp/cookies.txt
 cd ..
 
 mkdir tts_models
